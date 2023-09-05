e32fa4a3d95e38e7ecb8bab79aaf9a74e71405a8
kasmil <kamilsalikhov75@gmail.com>
2023-09-05 21:02:24 +0300

    Добавлен компонент спиннер

---
+++ src/shared/ui/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import { Spin, SpinProps } from "antd";
+import { FC } from "react";
+
+interface ISpinnerProps extends SpinProps {}
+
+export const Spinner: FC<ISpinnerProps> = (props) => {
+  const { children, ...otherProps } = props;
+  
+  return <Spin {...otherProps}> {children} </Spin>;
+};

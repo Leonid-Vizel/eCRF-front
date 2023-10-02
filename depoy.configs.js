e732c3c731abaffecede2276619763d29f5f6622
Alexnobody112 <Alsergtrofimov@gmail.com>
2023-10-02 21:49:24 +0300

    auto-deploy настроен автодеплой

---
+++ depoy.configs.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dev: {
+    host: '217.18.63.242',
+    username: 'root',
+    password: 'wYU_cZRQ7-m3-+',
+    port: 22,
+    relativePath: 'dev.ecrf.bioequivalencetrials.ru',
+  },
+  prod: {
+    host: '90.156.226.50',
+    username: 'root',
+    password: 'rQ1,e.C.JkxxR-',
+    port: 22,
+    relativePath: 'ecrf.bioequivalencetrials.ru',
+  },
+};
--- src/app/providers/ThemeProvider/ui/RootStylesProvider.tsx
+++ src/app/providers/ThemeProvider/ui/RootStylesProvider.tsx
@@ -3,7 +3,6 @@ import { FC } from 'react';
 import ru_RU from 'antd/lib/locale/ru_RU';
 import 'dayjs/locale/ru';
 
-
 interface RootStylesProviderProps {
   children?: React.ReactNode;
 }

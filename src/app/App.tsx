6806dae8d81e87e027ad3abc82af89bcdbbd02a4
Alexnobody112 <Alsergtrofimov@gmail.com>
2023-10-05 22:42:15 +0300

    ci-cd cicd test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,10 +9,10 @@ function App() {
     dispatch(retrieveDataFromCookie());
   }, []);
   return (
-    <>
-      <h1>hello world</h1>
-      <AppRouter />
-    </>
+  // <>
+  // <h1>hello world</h1>
+    <AppRouter />
+  // </>
   );
 }
 export default App;

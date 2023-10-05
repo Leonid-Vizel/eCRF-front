c3f0292ec38a05dfce29f7f9c5f1a77046f0f178
Alexnobody112 <Alsergtrofimov@gmail.com>
2023-10-05 23:23:21 +0300

    ci-cd fix absolute paths v3

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
   return {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.jsx', '.ts', '.js'],
     preferAbsolute: true,
     modules: [options.paths.src, 'node_modules'],
     alias: {src: path.resolve(__dirname, 'src')},

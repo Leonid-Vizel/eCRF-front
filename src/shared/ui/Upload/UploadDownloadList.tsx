efb1d9f31fd6db4b8aa54ede9c26de5ec337e135
kasmil <kamilsalikhov75@gmail.com>
2023-10-30 18:26:57 +0300

    TASK-93 фикс uploadDownloadList

--- src/shared/ui/Upload/UploadDownloadList.tsx
+++ src/shared/ui/Upload/UploadDownloadList.tsx
@@ -1,29 +1,47 @@
 import { Upload } from 'antd';
 import { FC, useEffect, useState } from 'react';
 import { externalSystemCall } from 'shared/api';
+import { Button } from '../Button';
 
 interface UploadDownloadListProps {
   listAction: string;
   uploadAction: string;
   deleteAction: string;
+  accept?:string
 }
 
-export const UploadDownloadList: FC<UploadDownloadListProps> = ({ listAction, uploadAction, deleteAction }) => {
+export const UploadDownloadList: FC<UploadDownloadListProps> = ({
+  listAction, uploadAction, deleteAction, accept = '.pdf',
+}) => {
   const [fileList, setFileList] = useState([]);
 
   useEffect(() => {
-    externalSystemCall({ method: 'GET', endpoint: listAction }).then((response) => setFileList(response.data));
+    externalSystemCall({ url: '', method: 'GET', endpoint: listAction }).then((response) => {
+      const files = response.data.map((file) => ({ uid: file.id, name: file.name, url: file.linkFile }));
+      setFileList(files);
+    });
   }, [listAction]);
 
   return (
     <Upload
+      accept={accept}
       action={uploadAction}
       onRemove={(file) => {
-        console.log(file.uid, deleteAction);
-        externalSystemCall({ method: 'DELETE', endpoint: deleteAction.replace('{index}', file.uid) });
+        externalSystemCall({ url: '', method: 'POST', endpoint: deleteAction.replace('{index}', file.uid) });
       }}
-      onChange={(info) => console.log(info)}
-      defaultFileList={fileList}
-    />
+      onChange={(info) => {
+        if (info.file.status === 'done') {
+          externalSystemCall({ url: '', method: 'GET', endpoint: listAction }).then((response) => {
+            const files = response.data.map((file) => ({ uid: file.id, name: file.name, url: file.linkFile }));
+            setFileList(files);
+          });
+        } else {
+          setFileList(info.fileList);
+        }
+      }}
+      fileList={fileList}
+    >
+      <Button>Загрузить</Button>
+    </Upload>
   );
 };
